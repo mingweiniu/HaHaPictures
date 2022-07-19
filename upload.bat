42eee83bae06303a2487cb40cb802949345bbb13
mingweiniu <mingweiniu@gmail.com>
2022-07-19 17:23:03 +0800

    all

--- upload.bat
+++ upload.bat
@@ -1,5 +1,7 @@
 git config --local user.email "mingweiniu@gmail.com"
 git config --local user.name "mingweiniu"
+git remote set-url origin git@SID:mingweiniu/HaHaPictures.git
+
 git pull
 git add .
 git commit -m "all"
